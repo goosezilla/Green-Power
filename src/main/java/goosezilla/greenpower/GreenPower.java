e5b7f30cdf007fc8af6e31e8114b5c5a2172c6c2
goosezilla <a.mcalpine88@gmail.com>
2016-07-26 14:57:32 +0100

    1

---
+++ src/main/java/goosezilla/greenpower/GreenPower.java
@@ -0,0 +1,92 @@
+package goosezilla.greenpower;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import goosezilla.greenpower.registry.*;
+
+import java.io.File;
+
+import static net.minecraftforge.oredict.OreDictionary.registerOre;
+
+
+/**
+ * Created by Goose on 25/07/2016. boo hoo
+ */
+
+    @Mod(modid = GreenPower.MODID, version = GreenPower.VERSION, dependencies = GreenPower.DEPEND,  name = GreenPower.MODNAME, acceptedMinecraftVersions = "[1.10.2]")
+    public class GreenPower {
+
+        public static final String MODID = "greenpower";
+        public static final String DEPEND = "";
+        public static final String VERSION = "1.10.2-0.1";
+        public static final String MODNAME = "GreenPower";
+        public static final String CLIENTPROXY = "goosezilla.greenpower.ClientProxy";
+        public static final String COMMONPROXY = "goosezilla.greenpower.CommonProxy";
+
+    @SidedProxy(clientSide = GreenPower.CLIENTPROXY, serverSide = GreenPower.COMMONPROXY)
+        public static CommonProxy proxy;
+
+    @Instance(MODID)
+        public static GreenPower instance;
+
+
+    @SideOnly(Side.CLIENT)
+    @EventHandler
+        public void initClient(FMLInitializationEvent event) {
+
+       //ModRecipes.init();
+
+        //Ores
+        registerOre("oreXPCrystal", new ItemStack(ModBlocks.BLOCK_XP_CRYSTAL, 1));
+
+        //Gems
+        registerOre("gemXPCrystal", new ItemStack(ModItems.XP_CRYSTAL, 1));
+
+    }
+
+    @SideOnly(Side.SERVER)
+    @EventHandler
+    public void initServer(FMLInitializationEvent event) {
+
+        //ModRecipes.init();
+
+        //Ores
+        registerOre("oreXPCrystal", new ItemStack(ModBlocks.BLOCK_XP_CRYSTAL, 1));
+
+        //Gems
+        registerOre("gemXPCrystal", new ItemStack(ModItems.XP_CRYSTAL, 1));
+
+    }
+
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ModItems.init();
+        ModBlocks.init();
+        ModBlocks.register();
+
+        proxy.registerRenderers(this);
+        proxy.registerWorldGenerators();
+    }
+
+//    @EventHandler
+//   public void serverLoad(FMLServerStartingEvent event) {
+//        event.registerServerCommand(new CommandGreenPower());
+//    }
+
+
+}
