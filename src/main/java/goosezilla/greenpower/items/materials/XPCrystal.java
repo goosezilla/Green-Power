c836714d7fb21d87856a31a92ec469f9fbbf91ff
goosezilla <a.mcalpine88@gmail.com>
2016-07-27 19:33:35 +0100

    1

--- src/main/java/goosezilla/greenpower/items/materials/XPCrystal.java
+++ src/main/java/goosezilla/greenpower/items/materials/XPCrystal.java
@@ -1,11 +1,18 @@
 package goosezilla.greenpower.items.materials;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResult;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import goosezilla.greenpower.GreenPower;
+import net.minecraft.entity.item.EntityXPOrb;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 
 
 /**
@@ -18,6 +25,13 @@ public class XPCrystal extends Item {
         setUnlocalizedName("XPCrystal");
         GameRegistry.register(this);
         //this.setCreativeTab(GreenPower.TAB_GREENPOWER_ITEMS)
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(ItemStack stack, World world, EntityPlayer player, EnumHand hand){
+        world.spawnEntityInWorld(new EntityXPOrb(world, player.posX+0.5, player.posY+0.5, player.posZ+0.5, 20));
+        stack.stackSize--;
+        return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, stack);
     }
 
     @SideOnly(Side.CLIENT)
