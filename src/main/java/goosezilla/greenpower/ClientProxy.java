f68e05365a9defaffad38b01c2cded6a60761724
goosezilla <goosezilla@users.noreply.github.com>
2016-08-01 22:28:09 +0100

    borked pig render code

--- src/main/java/goosezilla/greenpower/ClientProxy.java
+++ src/main/java/goosezilla/greenpower/ClientProxy.java
@@ -1,9 +1,14 @@
 package goosezilla.greenpower;
 
+import goosezilla.greenpower.entity.EntityGreenPig;
+import goosezilla.greenpower.render.RenderEntityGreenPig;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import java.util.HashMap;
@@ -16,6 +21,10 @@ public class ClientProxy extends CommonProxy
     @Override
     public void preInit(FMLPreInitializationEvent e) {
         super.preInit(e);
+
+        //@SuppressWarnings("unused")
+        //RenderManager rm = Minecraft.getMinecraft().getRenderManager();
+        //RenderingRegistry.registerEntityRenderingHandler(EntityGreenPig.class, RenderEntityGreenPig::new);
 
         for(Map.Entry<ItemStack, ModelResourceLocation> entry : MODEL_LOCATIONS.entrySet()){
             ModelLoader.setCustomModelResourceLocation(entry.getKey().getItem(), entry.getKey().getItemDamage(), entry.getValue());
