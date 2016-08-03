33eab517ddfc8142046c27cf0a837a64988cd8bd
pobiega <robin.aakerman@gmail.com>
2016-08-03 15:50:50 +0200

    Added the mutation potion to mutate pigs into green pigs.

--- src/main/java/goosezilla/greenpower/registry/ModItems.java
+++ src/main/java/goosezilla/greenpower/registry/ModItems.java
@@ -2,6 +2,7 @@ package goosezilla.greenpower.registry;
 
 import goosezilla.greenpower.items.materials.ItemGreenCoal;
 import goosezilla.greenpower.items.materials.ItemGreenIron;
+import goosezilla.greenpower.items.materials.ItemMutationPotion;
 import goosezilla.greenpower.items.materials.ItemXPCrystal;
 import net.minecraft.item.Item;
 
@@ -10,12 +11,13 @@ public class ModItems
     public static Item itemGreenIron;
     public static Item itemXPCrystal;
     public static Item itemGreenCoal;
+    public static Item itemMutationPotion;
 
     public static void init()
     {
         itemGreenIron = new ItemGreenIron("itemGreenIron");
         itemXPCrystal = new ItemXPCrystal("itemXPCrystal");
         itemGreenCoal = new ItemGreenCoal("itemGreenCoal");
-
+        itemMutationPotion = new ItemMutationPotion("itemMutationPotion");
     }
 }
